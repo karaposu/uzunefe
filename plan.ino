a435adc9edd38a20eaba7da555cbbfc051de6c31
Enes <31979644+karaposu@users.noreply.github.com>
2019-03-04 23:12:25 +0300

    Delete plan.ino

--- plan.ino
+++
@@ -1,4 +0,0 @@
-//mesela 32 derecede öne egik hale geldi. çıkış 255 verdi. kendini toparlamak istiyor
-//
-//bunun için geri gelicek. bunu yaparken eger yeterli ise  açı azalıyor git gde açı 10 a geldi bir momentum var normalde 10 derece olsaydı 55 pwm çıkışı alıcaktık 
-//ama momentum oldugundan dolayı geri düşecek bu sebeble -55 lik bir pwm vermesi boylee dengelemesi gerekiyor
